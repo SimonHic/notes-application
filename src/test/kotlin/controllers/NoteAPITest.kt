4aa04d0ef44245e75aa292fe8d91c6a358ce7df5
SimonHic <simonhicdev@gmail.com>
2023-03-17 20:35:46 +0000

    Add delete functionality with tests for specific situations

--- src/test/kotlin/controllers/NoteAPITest.kt
+++ src/test/kotlin/controllers/NoteAPITest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class NoteAPITest {
 
@@ -174,4 +175,27 @@ class NoteAPITest {
 
     }
 
+    /**Tests for deleteNotes include:
+     * Return null when trying to delete a note that is null
+     * Return deleted object from existing note
+     * */
+    @Nested
+    inner class DeleteNotes {
+
+        @Test
+        fun `deleting a Note that does not exist, returns null`() {
+            assertNull(emptyNotes!!.deleteNote(0))
+            assertNull(populatedNotes!!.deleteNote(-1))
+            assertNull(populatedNotes!!.deleteNote(5))
+        }
+
+        @Test
+        fun `deleting a note that exists delete and returns deleted object`() {
+            assertEquals(5, populatedNotes!!.numberOfNotes())
+            assertEquals(swim, populatedNotes!!.deleteNote(4))
+            assertEquals(4, populatedNotes!!.numberOfNotes())
+            assertEquals(learnKotlin, populatedNotes!!.deleteNote(0))
+            assertEquals(3, populatedNotes!!.numberOfNotes())
+        }
+    }
 }
