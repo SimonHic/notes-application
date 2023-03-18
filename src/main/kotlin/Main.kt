5c24ef71216bb0fd9445173f57e44fa4812f7a13
SimonHic <simonhicdev@gmail.com>
2023-03-18 18:02:11 +0000

    Add JSON Persistence to save in a different format

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import controllers.NoteAPI
 import models.Note
 import mu.KotlinLogging
+import persistence.JSONSerializer
 import persistence.XMLSerializer
 import utils.ScannerInput
 import utils.ScannerInput.readNextInt
@@ -9,7 +10,10 @@ import java.io.File
 import java.lang.System.exit
 
 private val logger = KotlinLogging.logger {}
-private val noteAPI = NoteAPI(XMLSerializer(File("notes.xml")))
+
+/**Uncomment and comment to alternate between the two*/
+//private val noteAPI = NoteAPI(XMLSerializer(File("notes.xml")))
+private val noteAPI = NoteAPI(JSONSerializer(File("notes.json")))
 
 
 
